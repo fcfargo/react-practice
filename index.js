d92d12a299c38cac210f712e6b052ab4535dfb30
fcfargo <fcfargo90@gmail.com>
2024-06-03 16:25:57 +0900

    feat: set google strategy options

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
+require('dotenv').config({ path: `${__dirname}/config/.env` });
 
 const app = express();
 
 app.get('/health-check', (req, rest) => {
-  rest.send({ hi: 'there' });
+  rest.send('ok');
 });
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log(accessToken);
+    }
+  )
+);
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server Listen Port ${PORT}`));
