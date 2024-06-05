156887c1ac1122cc96c3e6cb7fffcb99566ab69e
fcfargo <fcfargo90@gmail.com>
2024-06-05 12:48:27 +0900

    feat: add logout router

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,4 +11,9 @@ module.exports = (app) => {
   app.get('/api/users/current', (req, res) => {
     res.send(req.user);
   });
+
+  app.get('/api/users/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
