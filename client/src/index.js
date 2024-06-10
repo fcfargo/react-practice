37653d2c7c9f2410d1e8b272d08a42da39e06908
fcfargo <fcfargo90@gmail.com>
2024-06-10 16:08:59 +0900

    feat: set reducer in store

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import { applyMiddleware, createStore } from 'redux';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
