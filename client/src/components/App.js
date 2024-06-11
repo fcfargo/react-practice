5acaf8374d3369b5da0659361526e4692395e74a
fcfargo <fcfargo90@gmail.com>
2024-06-11 17:19:34 +0900

    feat: add react router and header layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+
+import Header from './Header';
+
+const SuervyNew = () => <h2>ServeyNew</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <div>
+                <Header />
+                <Outlet />
+              </div>
+            }
+          >
+            <Route path="/" element={<Landing />} />
+            <Route path="/surveys" element={<Dashboard />} />
+            <Route path="/surveys/new" element={<SuervyNew />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
