d218b5ae21836b3972de4a50dd8809f06e7a182e
fcfargo <fcfargo90@gmail.com>
2024-06-12 16:14:47 +0900

    refactor: refactor fetchUser function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return async function (dispatch) {
-    const response = await axios.get('/api/users/current');
+export const fetchUser = () => async (dispatch) => {
+  const response = await axios.get('/api/users/current');
 
-    const action = {
-      type: FETCH_USER,
-      payload: response,
-    };
-
-    return dispatch(action);
+  const action = {
+    type: FETCH_USER,
+    payload: response,
   };
+
+  dispatch(action);
 };
